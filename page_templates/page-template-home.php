852a1a25d12e51f2090fcfb02f5787aeafbd9ac9
Pedro Ecsedei <pedro.martins55757@gmail.com>
2024-02-06 03:43:41 -0300

    faq section

--- page_templates/page-template-home.php
+++ page_templates/page-template-home.php
@@ -5,6 +5,7 @@
     get_template_part('partials\home\banner');
     get_template_part('partials\home\differences');
     get_template_part('partials\home\services');
+    get_template_part('partials\home\faq');
 
     get_footer();
 ?>
---
+++ partials/home/faq.php
@@ -0,0 +1,50 @@
+<section class="faq">
+    <div class="container">
+        <div class="esq">
+            <h3><?php the_field('title'); ?></h3>
+            <p><?php the_field('text'); ?></p>
+            
+            <?php  
+                $linkFaqButton= get_field('faq_button_link');
+            
+                if( $linkFaqButton ) { 
+                    $linkFaqButton_url = $linkFaqButton['url'];
+                    $linkFaqButton_title = $linkFaqButton['title'];
+                    $linkFaqButton_target = $linkFaqButton['target'] ? $linkFaqButton['target'] : '_self';
+                    
+                    echo <<<FAQBUTTON
+                        <a class="button" href="$linkFaqButton_url" target="$linkFaqButton_target">
+                            $linkFaqButton_title
+                        </a>
+                    FAQBUTTON;
+                } 
+            ?>
+        </div>
+
+        <div class="faqs">
+            <?php if( have_rows('faqs') ): while( have_rows('faqs') ) : the_row(); ?>
+                <div class="faqs__item">
+                    <div class="faqs__item--header">
+                        <?php the_sub_field('faq_title'); ?>
+                        <div class="open-close-icon"></div>
+                    </div>
+
+                    <div class="faqs__item--content">
+                        <?php the_sub_field('editor'); ?>
+                    </div>
+                </div>
+            <?php endwhile; endif; ?>
+        </div>
+    </div>
+</section>
+
+<script>
+    (function($) {
+        $('.faqs__item--content').hide();
+
+        $('.faqs__item--header').on('click', function() {
+            $(this).next('.faqs__item--content').slideToggle(300);
+            $(this).find('.open-close-icon').toggleClass('active');
+        });
+    })(jQuery);
+</script>
